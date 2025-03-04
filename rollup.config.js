7caf6cd5ff807e24947168f514106dae9b815b64
Gwanghun-Im <lgh82229047@gmail.com>
2025-03-04 15:04:55 +0900

    fix: simplify rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,34 +1,23 @@
 const typescript = require("@rollup/plugin-typescript")
 const resolve = require("@rollup/plugin-node-resolve")
 const svgr = require("@svgr/rollup")
-const dts = require("rollup-plugin-dts")
 
-const config = [
-  {
-    input: "src/index.ts",
-    output: {
-      dir: "dist",
-      format: "cjs",
-      sourcemap: true,
-    },
-    external: ["react", "react-dom"],
-    plugins: [
-      resolve(),
-      svgr(),
-      typescript({
-        tsconfig: "./tsconfig.json",
-        exclude: ["**/*.test.ts", "**/*.test.tsx"],
-      }),
-    ],
+const config = {
+  input: "src/index.ts",
+  output: {
+    dir: "dist",
+    format: "cjs",
+    sourcemap: true,
   },
-  {
-    input: "src/index.ts",
-    output: {
-      file: "dist/index.d.ts",
-      format: "es",
-    },
-    plugins: [dts()],
-  },
-]
+  external: ["react", "react-dom"],
+  plugins: [
+    resolve(),
+    svgr(),
+    typescript({
+      tsconfig: "./tsconfig.json",
+      exclude: ["**/*.test.ts", "**/*.test.tsx"],
+    }),
+  ],
+}
 
 module.exports = config
